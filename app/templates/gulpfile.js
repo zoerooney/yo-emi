f784b2e1e9b5802d32a66a0dc8ed9702602a0061
Zoe Rooney <zoe@zoerooney.com>
2014-03-19 13:37:04 -0400

    updates to gulp files

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -3,10 +3,8 @@ var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
-    rename = require('gulp-rename'),
     newer = require('gulp-newer'),
     imagemin = require('gulp-imagemin'),
-    notify = require('gulp-notify'),
     livereload = require('gulp-livereload'),
     lr = require('tiny-lr'),
     server = lr();
@@ -23,12 +21,9 @@ gulp.task('styles', function(){
 	    .pipe(minifycss())
 	    .pipe(gulp.dest(''))
 	    .pipe(livereload(server));
-
 });
 
 gulp.task('images', function() {
-  
-   
   return gulp.src(imgSrc, {base: 'assets/images/originals'})
         .pipe(newer(imgDest))
         .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
